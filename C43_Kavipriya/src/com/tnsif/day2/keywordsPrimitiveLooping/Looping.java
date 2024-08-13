24f131513e426fef5d3efd7de445f70ab8e70a2e
NAVEEN KUMAR <nk946432@gmail.com>
2024-08-13 21:55:28 +0530

    commit 2

--- C43_Kavipriya/src/com/tnsif/day2/keywordsPrimitiveLooping/Looping.java
+++ C43_Kavipriya/src/com/tnsif/day2/keywordsPrimitiveLooping/Looping.java
@@ -4,10 +4,11 @@ package com.tnsif.day2.keywordsPrimitiveLooping;
 public class Looping {
 	public static void main(String[] args) 
 	{	
-		for(int i = 1; i<=1000 ; i++)   //(int i = 1000; i<=1;i--)
+		for(int i = 1; i<=50 ; i++)   //(int i = 1000; i<=1;i--)
 		{	
 			System.out.print("Value of i:");
 			System.out.println(i);
 		}
+		System.out.println("welcome to GitHub");
 	}
 }
