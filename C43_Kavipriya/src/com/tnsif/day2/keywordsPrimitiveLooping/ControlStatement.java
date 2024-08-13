5142a5911abe9d3c2d7b85897e7716c9e28304f6
NAVEEN KUMAR <nk946432@gmail.com>
2024-08-13 12:22:12 +0530

    commit 1

---
+++ C43_Kavipriya/src/com/tnsif/day2/keywordsPrimitiveLooping/ControlStatement.java
@@ -0,0 +1,19 @@
+package com.tnsif.day2.keywordsPrimitiveLooping;
+
+public class ControlStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=6;
+		if(x!=5)
+		{
+			System.out.println("the value of x is not 5");
+		}
+		else
+		{
+			System.out.println("The value of x is 5");
+		}
+
+	}
+
+}
---
+++ C43_Kavipriya/src/com/tnsif/day2/keywordsPrimitiveLooping/Keyword.java
@@ -0,0 +1,10 @@
+package com.tnsif.day2.keywordsPrimitiveLooping;
+
+public class Keyword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
