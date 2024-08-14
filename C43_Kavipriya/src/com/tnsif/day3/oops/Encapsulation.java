932dfefc5ae7fa7fe20386bc031e3bba4c3e8cbe
NAVEEN KUMAR <nk946432@gmail.com>
2024-08-14 10:47:26 +0530

    commit 5

---
+++ C43_Kavipriya/src/com/tnsif/day3/oops/Encapsulation.java
@@ -0,0 +1,39 @@
+package com.tnsif.day3.oops;
+
+public class Encapsulation {
+
+
+        String name;
+        int age;
+        String dept;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name; //state the current object
+		}
+		public int getAge() {
+			return age;
+		}
+		public void setAge(int age) {
+			this.age = age;
+		}
+		public String getDept() {
+			return dept;
+		}
+		public void setDept(String dept) {
+			this.dept = dept;
+		}
+		
+		public static void main(String[] args) {
+			//class name  obj name=keyword refer name
+			Encapsulation obj = new Encapsulation();
+			obj.setName("kavi");
+			obj.setAge(20);
+			obj.setDept("CSE");
+		 System.out.println(obj.age);
+		}
+        
+	}
+
+
